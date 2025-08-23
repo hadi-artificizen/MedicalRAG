160cd628af7da84785bfdac356b864640d53c707
Abdul Hadi <abdulhadizaeem7@gmail.com>
2025-08-23 19:44:18 +0500

    2nd

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["localhost", "http://localhost:8501", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
